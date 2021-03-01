7a794977118891023ee2c366463f06b7738feab4
Garfield_zhang <929780650@qq.com>
2021-03-01 21:00:28 +0800

    extend

--- ts/03_class.ts
+++ ts/03_class.ts
@@ -1,9 +1,84 @@
-class Person{
-  name: string = "garfield";
-  static age: number = 18;
-  readonly sex: number = 1;
+// class Person{
+//   name: string = "garfield";
+//   static age: number = 18;
+//   readonly sex: number = 1;
+
+//   sayHello(){
+//     console.log("hello, my name is "+this.name);
+//   }
+// }
+// const per = new Person();
+// console.log(Person.age);    // 18
+// console.log(per.name);      // garfield
+// per.sayHello()              // hello, my name is garfield
+
+
+
+// 构造函数
+// class Dog{
+//   name: string;
+//   age: number;
+
+//   // 构造函数
+//   constructor(name: string, age: number){
+//     this.name = name;
+//     this.age = age;
+//   }
+
+
+//   bark(){
+//     alert("wang wang wang!!!")
+//   }
+// }
+
+// const dog = new Dog("zhangsan", 2);
+// const dog2 = new Dog("lisi", 4);
+// console.log(dog);
+// console.log(dog2);
+
+
+
+// 类的继承
+class Animal{
+  name: string;
+  age: number;
+
+    constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello(){
+    console.log("hahaha");
+    
+  }
 }
-const per = new Person();
-console.log(Person.age);
-console.log(per.name);
+
+class Dog extends Animal{
+  sayHello(){
+    console.log("wang wang wang!!!");
+    
+  }
+}
+
+class Cat extends Animal{
+  sayHello(){
+    console.log("miao~~~~~");
+  }
+}
+
+const dog = new Dog("rango", 2)
+const cat = new Cat("Rach", 4)
+dog.sayHello()
+console.log(dog);
+cat.sayHello()
+console.log(cat);
+
+
+
+
+
+
+
+
 
