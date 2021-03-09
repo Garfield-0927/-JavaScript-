da95400fd1e00e828da2b223d64aeb11eb1e5860
Garfield_zhang <929780650@qq.com>
2021-03-09 13:26:46 +0800

    prototype 3.9

--- js/原型链.js
+++ js/原型链.js
@@ -1,22 +1,67 @@
-var a = new String("abc")
-var b = new Number(111)
-var c = new Object({
-  key:123,
-})
-console.log(a);
-console.log(a.__proto__===String.prototype);
-console.log(b);
-console.log(b.__proto__===Number.prototype);
-console.log(c);
+// var a = new String("abc")
+// var b = new Number(111)
+// var c = new Object({
+//   key:123,
+// })
+// console.log(a);
+// console.log(a.__proto__===String.prototype);
+// console.log(b);
+// console.log(b.__proto__===Number.prototype);
+// console.log(c);
 
-function Person(name,age){
-  this.name = name;
-  this.age = age;
-}
+// function Person(name,age){
+//   this.name = name;
+//   this.age = age;
+// }
 
-Person.prototype.greet = function(){
-  alert("Hi, my name is "+this.name+", I am "+this.age+" years old!")
-}
-var person = new Person("garfield",18);
-person.greet();
-console.log(person.__proto__ === Person.prototype);
+// Person.prototype.greet = function(){
+//   alert("Hi, my name is "+this.name+", I am "+this.age+" years old!")
+// }
+// var person = new Person("garfield",18);
+// person.greet();
+// console.log(person.__proto__ === Person.prototype);
+
+
+
+// function Foo(){
+//   console.log("这是Foo函数");
+// }
+// Foo.prototype = { /* */ }
+
+// // 修复丢失的constructor属性
+// Object.defineProperty(Foo.prototype, "constructor", {
+//   enumerable: false,
+//   writable: true,
+//   configurable: true,
+//   value: Foo    // 让constructor指向Foo
+// })
+
+// var a = new Foo();
+// console.log(a.constructor === Foo);   // true
+// console.log(a.constructor === Object);    // false
+
+
+// function Supermarket(){}
+// Supermarket.prototype.product = "mask";
+
+// // Shop.__proto__ === Supermarket.prototype
+// function Shop(){}
+// Shop.prototype = new Supermarket();
+
+// // person.__proto__ === Shop.prototype
+// var person = new Shop();
+// console.log(person.product);      // mask
+
+
+// var arr = ["我","不","想","开","","学"];
+// console.log(arr instanceof Object);
+// console.log(arr);
+
+var F = function(){};
+
+Object.prototype.a = function(){};
+// Function.prototype.b = function(){};
+F.prototype.b = function(){};
+var f = new F();
+console.log(f.a);
+console.log(f.b);
